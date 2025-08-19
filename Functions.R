85e5c109a45203eeedba115ab0a2115bcbde319f
Ashleigh Walton <waltonashleigh23@gmail.com>
2025-08-19 14:33:11 -0400

    2.5 done

--- Functions.R
+++ Functions.R
@@ -5,6 +5,7 @@ new_mean <- function (x) {
 	return(mean_value)
 }
 
+#test
 x <- c(1, 2, 3, 4, 5, 6)
 new_mean(x)
 
@@ -15,12 +16,40 @@ new_mean(y)
 x <- 3
 x^2
 
-square <- function(x) {
-	n <- length(x)
-	square_value <- (x)^2
+square <- function(z) {
+	square_value <- z*z
 	return(square_value)
 }
 
-#test it out
+#test
 square(53)
 53^2
+
+#Add function
+add_two_numbers <- function(x, y) {
+	add_value <- x + y
+	return(add_value)
+}
+
+#test
+add_two_numbers(4, 6)
+add_two_numbers(5, 10)
+
+#Raise function
+raise <- function(x, power){
+	raise_value <- (x)^power
+	return(raise_value)
+}
+
+#test
+raise(x = 2, power = 4)
+2^4
+raise(x = 5, power = 3)
+5^3
+
+#Adjust function so if power is not given, x is squared
+raise <- function(x, power = 2){
+	raise_value <- (x)^power
+	return(raise_value)
+}
+raise(x = 5)
