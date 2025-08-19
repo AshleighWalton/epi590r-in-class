0244442b88cb90ea7cdd5af7957ab642e2ce3906
Ashleigh Walton <waltonashleigh23@gmail.com>
2025-08-19 16:17:02 -0400

    exercise 2.6 done

--- Functions.R
+++ Functions.R
@@ -12,7 +12,7 @@ new_mean(x)
 y <- c(34563, 234, 2352, 7457, 865, 2534)
 new_mean(y)
 
-#Square function
+#Square function (exercise 2.4)
 x <- 3
 x^2
 
@@ -35,7 +35,7 @@ add_two_numbers <- function(x, y) {
 add_two_numbers(4, 6)
 add_two_numbers(5, 10)
 
-#Raise function
+#Raise function (exercise 2.5)
 raise <- function(x, power){
 	raise_value <- (x)^power
 	return(raise_value)
@@ -53,3 +53,32 @@ raise <- function(x, power = 2){
 	return(raise_value)
 }
 raise(x = 5)
+
+#Standard deviation function (exercise 2.6)
+std_dev <- function(x, na.rm = TRUE) {
+	# if na.rm is TRUE, remove NA
+	# if na.rm is FALSE, don't remove NA
+	if (na.rm) {
+		x <- na.omit(x)
+	}
+	denominator <- length(x) - 1
+	if (denominator <= 0) {
+		return(NA)
+	} else {
+		mean_x <- mean(x)
+		differences <- x - mean_x
+		squared_differences <- differences^2
+		numerator <- sum(squared_differences)
+		standard_dev <- sqrt(numerator / denominator)
+		return(standard_dev)
+	}
+}
+
+
+#test
+x <- c(345, 234, 789, 1234, 467, 899)
+std_dev(x)
+sd(x)
+
+std_dev(nlsy$income)
+sd(nlsy$income, na.rm = TRUE)
